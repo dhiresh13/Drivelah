215f97a1ef01c0af7d347caa15be9355707db36e
dhiresh13 <dhiresh13.work@gmail.com>
2021-02-20 01:43:11 +0530

    First Screen Complete

--- src/App.js
+++ src/App.js
@@ -1,44 +1,103 @@
+import React from "react";
 import "./App.css";
 import masterImage from "./assets/Group 2339.svg";
 import masterImage2x from "./assets/Group 2339.svg";
 import masterImage3x from "./assets/Group 2339.svg";
+import sponsor from "./assets/insuranceLogo-min.2ed44c50.png";
+import sponsortwo from "./assets/Image 8.png";
 
 import user from "./assets/user-circle-solid.svg";
 import down from "./assets/round-keyboard_arrow_down-24px.svg";
+import flex from "./assets/Group 2363.svg";
+import like from "./assets/Group 191.svg";
 
-function App() {
-	return (
-		<div className={"mobile-container"}>
-			<div className={"main-content"}>
-				<div className={"d-flex logo"}>
-					<div>
-						<img src={masterImage} />
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			onActive: false,
+		};
+	}
+	render() {
+		let { onActive } = this.state;
+		return (
+			<div className={"mobile-container"}>
+				<div className={"main-content"}>
+					<div className={"top-section"}>
+						<div className={"d-flex logo"}>
+							<div>
+								<img src={masterImage} />
+							</div>
+							<div style={{ marginBottom: "65px" }} className={"rightLogo"}>
+								<img src={user} />
+								<img style={{ marginLeft: "5px" }} src={down} />
+							</div>
+						</div>
+						<div className={"header"}>Rent your</div>
+						<br />
+						<div style={{ marginBottom: "10px" }} className={"header"}>
+							neighbour's car
+						</div>
+						<div className={"d-flex"}>
+							<input
+								style={{ marginBottom: "10px" }}
+								onFocus={() => this.setState({ onActive: true })}
+								className={"searchBar"}
+								placeholder={
+									onActive
+										? "Enter your location"
+										: "Search for your perfect rental car"
+								}
+							/>
+							<div className={"searchIcon"}>
+								<i class="fa fa-search"></i>
+							</div>
+						</div>
 					</div>
-					<div style={{ marginBottom: "65px" }} className={"rightLogo"}>
-						<img src={user} />
-						<img style={{ marginLeft: "5px" }} src={down} />
-					</div>
-				</div>
-				<div className={"header"}>Rent your</div>
-				<br />
-				<div style={{ marginBottom: "10px" }} className={"header"}>
-					neighbour's car
-				</div>
-				<div>
-					<input
-						className={"searchBar"}
-						placeholder={"Search for your perfect rental car"}
-					/>
-				</div>
+					<div className={"cardView"}>
+						<div className={"sponsors"}>
+							<div>
+								<span>Insurance provided by</span>
+								<img src={sponsor} />
+							</div>
+							<div>
+								<span>Approved by</span>
+								<img
+									height={40}
+									width={129}
+									style={{ objectFit: "cover" }}
+									src={sponsortwo}
+								/>
+							</div>
+						</div>
+						<h4 className={"subHeading"}>Featured cars</h4>
 
-				{/* <div className={"cardView"}>
-					<div className={"sponsors"}></div>
-					<div className={"subHeading"}>Featured cars</div>
-					<div className={"carousal"}></div>
+						<div>
+							<div className={"carousalImage"}>
+								<div className={"carousalBody"}>
+									<img src={flex} />
+									<img src={like} />
+								</div>
+							</div>
+							<div className={"carousal-title"}>
+								<div>
+									<p>Chuan Do Tan’s</p>
+								</div>
+								<div className={"d-flex carousal-rightTitle"}>
+									<i style={{ marginRight: "3px" }} class="fa fa-star" />
+									<p>4.89</p>
+									<label style={{ fontWeight: "500" }}>123 trips</label>
+								</div>
+							</div>
+							<label className={"carName"}>Car brand name</label>
+
+							<label className={"rate"}>$75 / day</label>
+						</div>
+
+						<button className={"viewButton"}>View all Cars</button>
+					</div>
 				</div>
-				<div>View All Cars</div> */}
-			</div>
-			{/* 
+				{/* 
 			<div className={"backArrow"}>Search</div>
 			<div className={"pills"}>
 				<div>Daily</div>
@@ -56,8 +115,9 @@ function App() {
 				<input />
 				<input />
 			</div><div><Button>SEARCH</Button></div> */}
-		</div>
-	);
+			</div>
+		);
+	}
 }
 
 export default App;
